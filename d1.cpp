38271a1bbdb9273166fd79944527c3cb577947e5
Navrose <navrosesinghjohal@gmail.com>
2024-08-19 10:31:44 -0400

    Added comments in first file about what I currently want to master. I think I should add this in my readme.md as well

---
+++ d1.cpp
@@ -0,0 +1,13 @@
+/* So I'm proficient in solving basic medium leetcodes problems
+ however there are 3 algos that I plan on mastering
+ 1. sliding window
+ 2.DFS
+ 3.BFS
+
+How I'll do them
+1. Understand how and why a algo works
+2. see solved solutions with that algo
+3. solve problems that are solved using that algo
+*/
+
+// SLIDING WINDOW

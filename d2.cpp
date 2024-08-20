dabddec50517b1d3c31895a962f6d464c78f323f
Navrose <navrosesinghjohal@gmail.com>
2024-08-20 09:41:40 -0400

    some sudo code added

--- d2.cpp
+++ d2.cpp
@@ -1,1 +1,12 @@
 // Sliding window problems
+// aim is to do 5 fixed size prob and 5 variable
+// Q1- Maximum sum subarray of size K
+
+// identification- array/string type, size, subarray/substring/etc
+
+// window size always = endofwindow(j)-startOfWindow(i)+1
+
+// start with i=0,j=0 and check
+//  j-i+1 < K -> j++
+//  j-i+1 > K -> i++
+//  j-i+1 = K -> maintian and calc code and j++,i++

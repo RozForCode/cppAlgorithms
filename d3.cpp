344caf3542666d64e3945f1e06d0a462ffc83b66
Navrose <navrosesinghjohal@gmail.com>
2024-08-22 14:31:23 -0400

    revision done

--- d3.cpp
+++ d3.cpp
@@ -39,3 +39,40 @@ vector<int> answer1(const vector<int> arr, int K)
     }
     return result;
 }
+
+// revision of answer on day4
+
+vector<int> answer(const vector<int> arr, int k)
+{
+    vector<int> negatives;
+    vector<int> result;
+    int n = arr.size();
+
+    for (int i = 0; i < k; i++)
+    {
+        if (arr[i] < 0)
+        {
+            negatives.push_back(arr[i]);
+        }
+    }
+    for (int i = k; i < n; i++)
+    {
+        if (negatives.size() == 0)
+        {
+            result.push_back(0);
+        }
+        else
+        {
+            result.push_back(negatives.front());
+        }
+        if (arr[i] < 0)
+        {
+            negatives.push_back(arr[i]);
+        }
+        if (arr[i - k] < 0)
+        {
+            negatives.erase(negatives.begin());
+        }
+    }
+    return result;
+}

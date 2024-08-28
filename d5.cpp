5a7aa7afe5951b0c6b0709606113ee6b45c5fd9c
Navrose <navrosesinghjohal@gmail.com>
2024-08-28 08:43:07 -0400

    d5 revision done

--- d5.cpp
+++ d5.cpp
@@ -57,3 +57,27 @@ vector<int> maxInArr(vector<int> arr, int k)
 
     return result;
 }
+vector<int> maxInArr2(const vector<int> arr, int k)
+{
+    int n = arr.size();
+    deque<int> dq;
+    vector<int> result;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (!dq.empty() && (dq.front() == i - k))
+        {
+            dq.pop_front();
+        }
+        if (!dq.empty() && (arr[dq.back()] <= arr[i]))
+        {
+            dq.pop_back();
+        }
+        dq.push_back(i);
+        if (i >= k)
+        {
+            result.push_back(arr[dq.front()]);
+        }
+    }
+    return result;
+}

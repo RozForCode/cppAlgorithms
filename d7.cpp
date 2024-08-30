814e4721565073f6e7d042f8087e887a150704a5
Navrose <navrosesinghjohal@gmail.com>
2024-08-30 11:27:30 -0400

    second version done using only map and general format to solve the problem

--- d7.cpp
+++ d7.cpp
@@ -21,7 +21,6 @@ while(current>k){remove calculation for i(currentWindowIndex);i++}
 #include <climits>
 #include <string.h>
 #include <unordered_map>
-#include <utility>
 using namespace std;
 
 int intialanswer(string s)
@@ -41,6 +40,27 @@ int intialanswer(string s)
             //     currentWindowIndex = //use pair to update the window index to the next element from s[i]
             // update pair[s[i]] with i thereby removing the previous second value of s[i]
         }
+    }
+    return maxLength;
+}
+
+// it can be solved without using pair by using map.find and just storing the index in map
+// gpt reviewed
+int intialanswer(string s)
+{
+    int currentWindowIndex = 0;
+    unordered_map<char, int> map;
+    int maxLength = INT_MIN;
+    int n = s.length();
+
+    for (int i = 0; i < n; i++)
+    {
+        if (map.find(s[i]) != map.end())
+        {
+            currentWindowIndex = max(currentWindowIndex, map[s[i]] + 1);
+        }
+        maxLength = max(maxLength, i - currentWindowIndex + 1);
+        map[s[i]] = i;
     }
     return maxLength;
 }

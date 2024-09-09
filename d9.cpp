a399bd3329e2ad771a2d1772dd1a0a149c9c9ae7
Navrose <navrosesinghjohal@gmail.com>
2024-09-09 14:48:04 -0400

    gpt correction corrected

--- d9.cpp
+++ d9.cpp
@@ -38,3 +38,45 @@ int answer1(string s, string t)
     }
     return minLength;
 }
+
+// gpt reviewed
+// size after auto loop will be equal to t.length()-1
+
+int answer1(string s, string t)
+{
+    unordered_map<char, int> map;
+    int n = s.length();
+    int currentWindowIndex = 0;
+    int minLength = INT_MAX;
+
+    // Initialize the map with frequencies of characters in t
+    for (char c : t)
+    {
+        map[c]++;
+    }
+    int required = map.size();
+    int formed = 0;
+
+    // Two pointer approach with sliding window
+    for (int r = 0; r < n; r++)
+    {
+        char c = s[r];
+
+        if (map.find(c) != map.end())
+        {
+            map[c]--;
+            if (map[c] == 0)
+            {
+                formed++;
+            }
+        }
+        // when formed == required means found a valid window
+
+        // if(formed==required){
+        //     minLength= min(minLength,r-currentWindowIndex+1);
+
+        // }
+        // okay told gpt that it was wrong here and it should be a while loop till we find a relevant letter
+        // also in case when the substring is in the latter section of the string this approach won't work with a while loop inside
+    }
+}

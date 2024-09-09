7a600458b443aed08d45f2d9df84a8dbc15129ea
Navrose <navrosesinghjohal@gmail.com>
2024-09-09 14:37:11 -0400

    first attempt -- (close)

--- d9.cpp
+++ d9.cpp
@@ -1,1 +1,40 @@
 // minimum window substring | variable size sliding window
+#include <iostream>
+#include <climits>
+#include <string.h>
+#include <unordered_map>
+using namespace std;
+// map[s[i]] = i;
+int answer1(string s, string t)
+{
+    unordered_map<char, int> map;
+    int n = s.length();
+    int currentWindowIndex = 0;
+    int size = 0;
+    int minLength = INT_MAX;
+
+    for (auto c : t)
+    {
+        map[c]++;
+        size++;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (map.find(s[i]) != map.end())
+        {
+            map[s[i]]--;
+            size--;
+        }
+        if (size == 0)
+        {
+            minLength = min(minLength, i - currentWindowIndex + 1);
+            currentWindowIndex++;
+            if (map.find(s[currentWindowIndex]) != map.end())
+            {
+                map[s[currentWindowIndex]]++;
+                size++;
+            }
+        }
+    }
+    return minLength;
+}

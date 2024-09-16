c39a1c5f48fe71f645a4f6dab6f02c3e3c332b52
Navrose <navrosesinghjohal@gmail.com>
2024-09-16 15:23:57 -0400

    general stuff about heap added

--- heap1.cpp
+++ heap1.cpp
@@ -1,1 +1,8 @@
-// now I'll start working on heap questions
+// Identificaion - 1. K, 2. Largest or smallest    1 and 2 together
+// k will be the size of heap - top of the heap will have the answer after calc
+// 2 types - min heap, max heap
+// k+smallest - max heap
+// k+largest  - min heap
+// pop top push
+// all heap questions are sorting questions - nlogk
+// heap make sorting complexity from nlogn (array) to nlogk

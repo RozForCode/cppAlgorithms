9f21e7c0100b3be74bd47327686d5347e1c4c678
Navrose <navrosesinghjohal@gmail.com>
2024-09-17 15:12:52 -0400

    day 2 for heap done

---
+++ heap2.cpp
@@ -0,0 +1,49 @@
+// q2 - k largest element
+
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+int answer1(int arr[], int n, int k)
+{
+    priority_queue<int, vector<int>, greater<int>> min;
+    for (int i = 0; i < n; i++)
+    {
+        min.push(arr[i]);
+        if (min.size() > k)
+        {
+            min.pop();
+        }
+    }
+    return min.top();
+}
+
+// q3 - sort a k sorted array or nearly sorted array
+// values in the given array differ from there complete sorted array
+// index value by k... so range 0-k or 0+k
+int *answerq3(int arr[], int n, int k)
+{
+    priority_queue<int, vector<int>, greater<int>> min;
+    int *ans = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        min.push(arr[i]);
+        if (min.size() > k)
+        {
+
+            ans[i - k] = min.top();
+            min.pop();
+        }
+    }
+    int j = 0;
+    while (!min.empty())
+    {
+        ans[n - k + j] = min.top();
+        min.pop();
+        j++;
+    }
+    return ans;
+}
+// heap is not sorted only the first element is in the right posiiton
+// whenever popped , heap reheapifies

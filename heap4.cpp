19e40d056e94a62b95b80c5ed0b776222da232ec
Navrose <navrosesinghjohal@gmail.com>
2024-09-19 12:51:47 -0400

    discussion of q5

---
+++ heap4.cpp
@@ -0,0 +1,8 @@
+// q5- Top K frequent numbers
+// initial thoughts- sounds like we have the find the largest frequency so min heap(we get rid of small ones) by storing pair
+// priority_queue<pair<int,pair<int,int>,vector<int>,greater<int>
+
+// from video - we have to use hash
+// hashing -  hashing refers to the process of converting input data (like a string or number) into a fixed-size value, typically called a hash code or hash value. This is done using a hash function.4
+// first I'll study about hash function, hash table, collision, and applucation of hashing
+// then move onto the q5

15f502c154d2d29d134c0488d297148fe4a3cb49
Navrose <navrosesinghjohal@gmail.com>
2024-10-03 14:05:06 -0400

    Questions added

---
+++ heapRevision.cpp
@@ -0,0 +1,39 @@
+// questions
+// q1 - kth smallest element
+
+// q2 - k largest element
+
+// q3 - sort a k sorted array or nearly sorted array
+
+// q4- K closest numbers to a given value
+
+// q5- Top K frequent numbers
+
+// q6 Frequency sort
+
+/*
+Problem Statement: Frequency Sort (Heap Problem)
+You are given an array of integers, and your task is to sort the array based on the frequency of each number.
+The numbers with a higher frequency should come first. If two numbers have the same frequency, the number with the higher value
+should come first. The goal is to return the array sorted according to these rules.
+*/
+
+// q7 K -closed points to origin
+
+// Given a list of points on a 2D plane, the task is to find the K closest points to the origin (0, 0). The distance between two points (x1, y1) and (x2, y2) is given by the Euclidean distance formula:
+// distance = sqrt(x1^2-x2^2 + y1^2-y2^2)
+// for this question it'll be sqrt(x1^2+y1^2) as we are talking about origin
+
+// q8 Connect Ropes to Minimise the Cost
+/*
+You are given multiple ropes with different lengths. Your task is to connect all the ropes into a single rope.
+Each time you connect two ropes, the cost of connecting them is equal to the sum of their lengths.
+You need to find the minimum cost required to connect all the ropes into one.
+
+*/
+
+// q9 Sum of Elements between k1 smallest and k2 smallest numbers
+/*
+Given an array of integers and two integers k1 and k2, the task is to find
+the sum of the elements between the k1-th smallest and the k2-th smallest numbers in the array.
+*/

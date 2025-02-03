7c05c6dac729fcb2c6a784f6f80e517ebc55a9d6
Navrose Johal <johnavro@sheridancollege.ca>
2025-02-03 15:34:46 -0500

    Starting with heap, cleared heap and pair and heap+pair doubts

--- heap2.cpp
+++ heap2.cpp
@@ -18,6 +18,7 @@ int answer1(int arr[], int n, int k)
     }
     return min.top();
 }
+
 // q3 - You are given a nearly sorted array where each element is at most k positions
 // away from its correct position in the sorted array. Your task is to completely sort the array.
 // q3 - sort a k sorted array or nearly sorted array
@@ -46,5 +47,26 @@ int *answerq3(int arr[], int n, int k)
     }
     return ans;
 }
-// heap is not sorted only the first element is in the right posiiton
-// whenever popped , heap reheapifies
+// heap is not sorted only the first element is in the right position
+// whenever popped , heap re-heaped
+
+vector<int> answer4(int arr[], int n, int k)
+{
+    vector<int> ans;
+    priority_queue<int, vector<int>, greater<int>> min;
+
+    for (int i = 0; i < n; i++)
+    {
+        min.push(arr[i]);
+        if (min.size() > k)
+        {
+            ans.push_back(min.top());
+            min.pop();
+        }
+    }
+    while (!min.empty())
+    {
+        ans.push_back(min.top());
+        min.pop();
+    }
+}

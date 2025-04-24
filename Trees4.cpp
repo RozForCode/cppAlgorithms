92d07ce236877123fa3e92da004f531b65488672
Navrose Johal <johnavro@sheridancollege.ca>
2025-04-23 22:10:51 -0400

    Got it

--- Trees4.cpp
+++ Trees4.cpp
@@ -1,2 +1,53 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+#include <stack>
+struct TreeNode
+{
+    int data;
+    TreeNode *right;
+    TreeNode *left;
+    TreeNode(int value)
+    {
+        this->data = value;
+        this->left = nullptr;
+        this->right = nullptr;
+        /*
+        other way to initialize
+        data(value)
+        left(nullptr)
+        right(nullptr)
+        */
+    }
+};
 
 // post order iterative traversal is tricky can be done with 2 stacks, or double pushes with one stack
+vector<int> postOrderIterative(TreeNode *root)
+{
+    vector<int> ans;
+    stack<TreeNode *> s;
+    TreeNode *curr = root;
+    TreeNode *visited = nullptr;
+
+    while (curr != nullptr || !s.empty())
+    {
+        while (curr != nullptr)
+        {
+            s.push(curr);
+            s.push(curr);
+            curr = curr->left;
+        }
+        curr = s.top();
+        s.pop();
+        if (visited == curr)
+        {
+            ans.push_back(curr->data);
+        }
+        else
+        {
+            visited = curr;
+        }
+        curr = curr->right;
+    }
+    return ans;
+}

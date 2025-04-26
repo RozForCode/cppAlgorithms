f8f493cd5f21030631be2eea237b218cdf6695cc
Navrose Johal <johnavro@sheridancollege.ca>
2025-04-25 20:17:29 -0400

    Starting level order traversal

---
+++ Trees5.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+#include <stack>
+struct TreeNode
+{
+    int data;
+    TreeNode *right;
+    TreeNode *left;
+    TreeNode(int value)
+    {
+        this->data = value;
+        this->left = nullptr;
+        this->right = nullptr;
+        /*
+        other way to initialize
+        data(value)
+        left(nullptr)
+        right(nullptr)
+        */
+    }
+};
+// Level Order Traversal

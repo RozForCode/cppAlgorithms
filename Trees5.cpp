e75e031c379dfd9685e9ad8b124b7effcc327f7b
Navrose Johal <johnavro@sheridancollege.ca>
2025-04-28 08:36:36 -0400

    Level Order iterative

--- Trees5.cpp
+++ Trees5.cpp
@@ -26,7 +26,14 @@ void levelOrderTraversal(TreeNode *root, int level, vector<vector<int>> &res)
 {
     if (!root)
         return;
+    // creates empty vector
+    // res[[emptyvector]]
+    // which is accessed in res[level] where level is the index of the added vector
+    if (res.size() <= level)
+        res.push_back({});
     res[level].push_back(root->data);
     levelOrderTraversal(root->left, level + 1, res);
     levelOrderTraversal(root->right, level + 1, res);
 }
+
+// level order traversal iterative
